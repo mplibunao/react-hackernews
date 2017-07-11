039eae0055ddf9b28c87b727ac94d3c9e28f4049
mplibunao <markpaololibunao@gmail.com>
2017-07-11 17:50:19 +0800

    Changed from using static data to fetching data through an API request to hn.agolia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+//const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 /*
 // Remember filter takes in value/item as parameter
@@ -50,16 +39,35 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
     /*
       The function is bound to the class and thus becomes a class method
       Need to bind class methods in the constructor; Binds `this` to class
     */
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result){
+    this.setState({ result });
+  }
+
+  fetchSearchTopStories(searchTerm){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
+  }
+
+  // Make API Request after initial rendering of component
+  componentDidMount(){
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onDismiss(id){
@@ -73,7 +81,12 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    console.log(this.state);
+    const { searchTerm, result } = this.state;
+
+    if (!result){
+      return null;
+    }
 
     return (
       <div className="page">
@@ -86,7 +99,7 @@ class App extends Component {
           </Search>
         </div>
           <Table
-            list={list}
+            list={result.hits}
             pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
