eb73985889cbb429daedb2d97ad69c2fedcfd89e
mplibunao <markpaololibunao@gmail.com>
2017-07-12 19:52:04 +0800

    Added type checking through propTypes as well as unit tests and snapshot tests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -159,6 +160,7 @@ class App extends Component {
   }
 }
 
+
 /*
   You can only use concise body functions if you only have one expression
   In this case, this element tree is considered as one
@@ -181,6 +183,13 @@ const Search = ({
       {children}
     </button>
   </form>
+
+Search.PropTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 const Table = ({ list, onDismiss }) =>
 <div className="table">
@@ -210,25 +219,43 @@ const Table = ({ list, onDismiss }) =>
   )}
 </div>
 
+Table.PropTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
 
-class Button extends Component {
-  render(){
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
 
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
-}
+const Button = ({onClick, className, children})=>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+
+Button.PropTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+  className: '',
+};
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import App, { Search, Button, Table} from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', ()=>{
+
+  /**
+   *  @ Default test for create-react-app
+   *  Renders the component and its children to a div element
+   */
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  /**
+   *  @ Snapshot Test
+   *  Makes a snapshot of your rendered component and runs this snapshot against future snapshots
+   *  Notifies you of any changes and asks if you want to update the snapshot
+   */
+  test('snapshots', ()=>{
+    const component = renderer.create(
+      <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
+
+describe('Search', ()=>{
+
+  it('renders', ()=>{
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+  });
+
+  test('snapshots', ()=>{
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Button', ()=>{
+
+  it('renders', ()=>{
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Give Me More</Button>, div);
+  });
+
+  test('snapshots', ()=>{
+    const component = renderer.create(
+      <Button>Give Me More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Table', ()=>{
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ]
+  };
+
+  /** 
+   *  @ Unit Test 
+   *  Use shallow to render component and assert that Table has two items
+   *  - Shallow renders the component without child components. You can make the test dedicated to one component
+   *  - Enzyme has two other rendering mechanisms: mount() and render()
+   *  Both creates instances of the parent component and all child components
+   *  However mount() gives you more access to the component lifecycle methods
+   * 
+   *  Rule of thumb:
+   *  - Always begin with a shallow test
+   *  - If componentDidMount() or componentDidUpdate() should be tested, use mount()
+   *  - If you want to test component lifecycle and children behavior, use mount()
+   *  - If you want to test children with less overhead than mount() and you are not interested in lifecycle methods, use render()
+   */ 
+  it('shows two items in list', ()=>{
+    const element =  shallow(
+      <Table {...props} />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
+
+  it('renders', ()=>{
+    const div = document.createElement('div');
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test('snapshots', ()=>{
+    const component = renderer.create(
+      <Table {...props} />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
